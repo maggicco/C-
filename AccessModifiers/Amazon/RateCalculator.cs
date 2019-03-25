9d4cdbd187f916500cd09e8fedc05918b1eba5e8
maggicco <maggicco83@hotmail.fr>
2019-03-25 08:36:43 +0100

    Access Modifiers

---
+++ AccessModifiers/Amazon/RateCalculator.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace Amazon
+{
+    public class RateCalculator
+    {
+        public int Calculate()
+        {
+            return 0;
+        }
+
+        internal object Calculate(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
