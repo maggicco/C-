7220e6356dca7289a759c21beb7760572f673461
maggicco <maggicco83@hotmail.fr>
2019-03-25 08:37:37 +0100

    Linq

---
+++ Linq/Linq/BookRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "Book 1", Price = 5 },
+                new Book() {Title = "Book 2", Price = 9.99f },
+                new Book() {Title = "Book 3", Price = 12 },
+                new Book() {Title = "Book 4", Price = 7 },
+                new Book() {Title = "Book 5", Price = 9 }
+            };
+        }
+    }
+}
