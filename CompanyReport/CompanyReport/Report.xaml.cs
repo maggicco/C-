79e37ca8614cbaaf4ea161c5e5aaa31a18a78939
maggicco <maggicco83@hotmail.fr>
2019-03-25 07:33:12 +0100

    wpf-docs application

---
+++ CompanyReport/CompanyReport/Report.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompanyReport
+{
+    /// <summary>
+    /// Logique d'interaction pour Report.xaml
+    /// </summary>
+    public partial class Report : Page
+    {
+        public Report()
+        {
+            InitializeComponent();
+        }
+
+        
+        public Report(object data) : this()
+        {
+            // Bind report data.
+            this.DataContext = data;
+        }
+
+
+    }
+}
